6fe008225608c6d69bd22d07bf6d71249e5af99d
mrcordle <mrcordle@gmail.com>
2018-01-31 21:25:12 -0500

    Lab 2 still missing attack or quit

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
-var quest = prompt("Do you want to play?")
+function startGame(){
+  var quest = prompt("Do you want to play?")
+  if(quest.toLowerCase() === "yes") {
+      name = prompt("What is your name?");
+      startCombat(name);
 
-if(quest === "yes") {
-    var name = prompt("What is your name?");
-}
-var userHealth = "40";
-var grantHealth = "10";
-var wins = 0;
-
-while(wins < 3 && userHealth >=0){
- 
-  var hp = Math.floor(Math.random() * 3);
-  var hpUser = Math.floor(Math.random() * 3);
-  var userHealth = userHealth - hpUser;
-  var grantHealth = grantHealth - hp;
-  console.log(name,"has", userHealth, "health left.");
-  console.log("You hit Grant with", hp, "hp!");
-  console.log("Grant has", grantHealth, "health left.");
-  if(grantHealth <= 0){
-     var wins = wins + 1;
-     var grantHealth = 10;
-}
-
-  if(userHealth <= 0){
-    console.log("You have been defeated")
   }
-    if(wins >= 3){
-    console.log("You have defeated Grant!")
-   
-    }
   
-}
+  }
+  
+  startGame();
+  function startCombat(userName){
+  var userHealth = "40";
+  var grantHealth = "10";
+  var wins = 0;
+  function getDamage(num){   
+       num = Math.floor(Math.random() * 5) + 1;
+       return num;
+  }
+  while(wins < 3 && userHealth > 0){
+   
+  var hpUser = getDamage();
+  var hp = getDamage();
+    var userHealth = userHealth - hpUser;
+    var grantHealth = grantHealth - hp;
+    console.log(userName + " has " + userHealth +" health left.");
+    console.log("You hit Grant with " + hp + " hp!");
+    console.log("Grant has "+ grantHealth+ " health left.");
+    if(grantHealth <= 0){
+       var wins = wins + 1;
+       var grantHealth = 10;
+  }
+  
+    if(userHealth <= 0){
+      console.log("You have been defeated")
+    }
+      if(wins >= 3){
+      console.log("You have defeated Grant!")
+     
+      }
+    
+  }
+  }

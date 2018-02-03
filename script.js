c66c348c084349148a1d184f35c1210efc0de5e8
mrcordle <mrcordle@gmail.com>
2018-02-03 16:41:09 -0500

    Part 3

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+class User{
+  constructor(user){
+    this.user = user;
+    this.userHealth = 40;
+    this.healsRemaining = 2;
+    this.wins = 0;
+  }
+  getDamage(){
+    return Math.floor(Math.random() * 2) + 1;
+  
+  }heal(){
+    return Math.floor(Math.random() * 10) + 1;
+  }
+}
+
+class Grant extends User{
+  constructor(){
+    super();
+    this.boss = "Grant";
+    this.grantHealth = 10;
+  }generateAttackDamage(){
+  return Math.floor(Math.random() * 5) + 1;
+  }
+  
+}
+
 
 
 function startGame(){
@@ -8,27 +34,36 @@ function startGame(){
 
   }
 }
-function getDamage(){
-  return Math.floor(Math.random() * 5) + 1;
-  
-  }
 
 function startCombat(userName){
-  var userHealth = "40";
-  var grantHealth = "10";
-  var wins = 0;
 
 
+var usr = new User();
+var boss = new Grant();
+var grantDamage = boss.generateAttackDamage();
+var damage = usr.getDamage();
+var wins = usr.wins;
+var userHealth = usr.userHealth;
+var grantHealth = boss.grantHealth;
+var heal = usr.heal();
+var healsRemaining = usr.healsRemaining;
 
-  while(wins < 3 && userHealth > 0){
-     var cont = prompt("Would you like to attack or quit")
+
+  while(wins < 5 && userHealth > 0){
+     var cont = prompt("Would you like to attack, heal or quit")
      if(cont.toLowerCase() === "quit"){
        console.log("You have quit the game.")
        return;
      }
+      if (cont.toLowerCase() === "heal" && healsRemaining > 0){
+      userHealth += heal;
+      healsRemaining --;
+      console.log(userHealth);
+    
+     }
      if(cont.toLowerCase() === "attack"){
-     userHealth -= getDamage();
-     grantHealth -= getDamage();
+     userHealth -= damage;
+     grantHealth -= grantDamage;
 
     console.log(userName + " has " + userHealth +" health left.");
     console.log("Grant has "+ grantHealth+ " health left.");
@@ -44,7 +79,7 @@ function startCombat(userName){
     if(userHealth <= 0){
       console.log("You have been defeated");
     }
-      if(wins >= 3){
+      if(wins >= 5){
       console.log("You have defeated Grant!");
       }
 }
